d0e4a4257a0b576afa71e0b01bb69edcf72446ac
Ivaylo Petrov <ivaylo@ivaylo-Studio-1555.(none)>
2012-10-20 20:12:41 +0300

    Add auto configure script

---
+++ setup_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+cur_dir=`pwd`
+
+ln -s $cur_dir/vimrc ~/.vimrc
+ln -s $cur_dir/tmux.conf ~/.tmux.conf
+ln -s $cur_dir/zshrc ~/.zshrc
+ln -s $cur_dir/xxkbrc ~/.xxkbrc
+ln -s $cur_dir/Xdefaults ~/.Xdefaults
+ln -s $cur_dir/vim ~/.vim
