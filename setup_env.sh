b931bc23aa9893a6c68e473f24d6301e1f36bf87
Ivaylo Petrov <ivajloip@gmail.com>
2013-06-05 20:12:14 +0300

    Making ugly fix for vundle installation - not tested yet

--- setup_env.sh
+++ setup_env.sh
@@ -10,3 +10,8 @@ ln -s $cur_dir/Xdefaults ~/.Xdefaults
 ln -s $cur_dir/vim ~/.vim
 ln -s $cur_dir/xprofile ~/.xprofile
 ln -s $cur_dir/gitconfig ~/.gitconfig
+
+# Fix for vundle install
+rmdir $cur_dir/vim/bundle/vundle
+cd $cur_dir/vim/bundle/
+git clone https://github.com/gmarik/vundle.git
