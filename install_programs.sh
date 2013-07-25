8f194b3ea319e939d9da4dd4e28907dee2cbb30c
Ivaylo Petrov <ivajloip@gmail.com>
2013-07-25 19:09:34 +0300

    Adding default terminator configuration to the install process

--- install_programs.sh
+++ install_programs.sh
@@ -6,4 +6,6 @@ bash setup_urxvt_clipboard.sh
 sudo apt-get install htop iotop tcpdump acpitool vim vim-gtk awesome \
   tmux rxvt-unicode xxkb git zsh xpra axel xnest xsel x11-utils network-manager-gnome
 
+sudo update-alternatives --config x-terminal-emulator
+
 bash setup_env.sh
